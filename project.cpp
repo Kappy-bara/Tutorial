3aaef55b6e5f47636f02828386e27c5f3c87b768
Kappy-bara <jnanadattanayak2003@gmail.com>
2025-04-14 11:28:22 +0530

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -1,6 +1,43 @@
 //============Project File================//
 #include<iostream>
+#include<vector>
 using namespace std;
-int main(){
 
+class Grid{
+    int row, col;
+    vector<vector<int>> grid;
+
+    public:
+    Grid(int x, int y){
+        row = x;
+        col = y;
+        grid.resize(row, vector<int>(col, 0));
+    }
+    void SetObstacle(int x, int y){
+        if(x>=0 && x<row && y>=0 && y<col){
+            grid[x][y] = 1;
+        }
+    }
+    void ClearObstacle(int x, int y){
+        if(x>=0 && x<row && y>=0 && y<col){
+            grid[x][y]=0;
+        }
+    }
+    void PrintGrid(){
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                cout<<grid[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+    }
+    void GenerateMaze(){
+        //Generate a maze in the grid.
+    }
+};
+int main(){
+    Grid g(5, 5);
+    g.SetObstacle(1, 1);
+    g.SetObstacle(2, 2);
+    g.PrintGrid();
 }
