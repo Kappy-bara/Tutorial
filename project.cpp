bc25650f6de19ddc05837be5072cc795142909c1
JD <jnanadattanayak2003@gmail.com>
2025-04-15 00:37:58 +0530

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -3,30 +3,33 @@
 #include<vector>
 using namespace std;
 
+template <class T>
 class Grid{
     int row, col;
-    vector<vector<int>> grid;
-
+    vector<vector<bool>> grid;
+    vector<vector<T>> tileCost;
     public:
     Grid(int x, int y){
         row = x;
         col = y;
-        grid.resize(row, vector<int>(col, 0));
+        grid.resize(row,vector<bool>(col, 0));
+        tileCost.resize(row,vector<T>(col,1));
     }
     void SetObstacle(int x, int y){
         if(x>=0 && x<row && y>=0 && y<col){
-            grid[x][y] = 1;
+            grid[x][y] = true;
         }
     }
     void ClearObstacle(int x, int y){
         if(x>=0 && x<row && y>=0 && y<col){
-            grid[x][y]=0;
+            grid[x][y] = false;
         }
     }
     void PrintGrid(){
         for(int i=0;i<row;i++){
             for(int j=0;j<col;j++){
-                cout<<grid[i][j]<<" ";
+                if(grid[i][j]) cout<<"X";
+                else cout<<"*";
             }
             cout<<endl;
         }
